acfc1afac995b19f5a1fa70164cc2e43ced25402
Kashyap Murali <katchu11@gmail.com>
2017-12-02 10:51:27 -0500

    FINAL REGEX

--- Backend/regex_function.py
+++ Backend/regex_function.py
@@ -1,12 +1,11 @@
 # coding=utf-8
 import re
-def regex_processing():
-	str= "Hi My name is Sheldon Cooper and I have 11 years of experience working with Accenture, I have mathematics and statistics background. I have completed my doctorate from MIT University. I have experience with auto domain and currently have CTC of $170000."
-
+def regex_processing(str):
 
 	name = re.findall(r'name is (\w+\s+\w+)', str) #Use Limit
 	print(name[0])
 	experience= re.findall(r'I have (\d{1,6})', str)
+	print(experience[0])
 	co = re.findall(r'working with ([\w\.-]+)',str)
 	print(co[0])
 	degree = re.findall(r'completed my ([\w\.-]+) from', str)
@@ -17,5 +16,4 @@ def regex_processing():
 	print(ctc[0])
 
 
-
-regex_processing()
+regex_processing("Hi My name is Sheldon Cooper and I have 11 years of experience working with Accenture, I have mathematics and statistics background. I have completed my doctorate from MIT University. I have experience with auto domain and currently have CTC of $170000.")
