b4ce25acc3996e573b87f3a52027e07960c935d3
Krishnan Ram <khrams100@gmail.com>
2017-12-02 10:47:52 -0500

    Full Regex Completion minus mapping

--- Backend/regex_function.py
+++ Backend/regex_function.py
@@ -1,19 +1,21 @@
 # coding=utf-8
 import re
 def regex_processing():
-	str= "Hi My name is Sheldon Cooper and I have 11 years of experience working with Accenture, I have mathematics and statistics background. I have completed my doctorate from MIT University. I have experience with auto domain and currently have CTC of  $170000."
+	str= "Hi My name is Sheldon Cooper and I have 11 years of experience working with Accenture, I have mathematics and statistics background. I have completed my doctorate from MIT University. I have experience with auto domain and currently have CTC of $170000."
 
 
-	name = re.search(r'name is \w+\s+\w+', str) #Use Limit
-	if name:
-		print ('found', name.group())
-	else:
-		print ('did not find')
-	experience= re.findall(r'\d{1,6}', str)
-	if experience:
-		print (experience[0])
-	else:
-		print ('did not find')
+	name = re.findall(r'name is (\w+\s+\w+)', str) #Use Limit
+	print(name[0])
+	experience= re.findall(r'I have (\d{1,6})', str)
+	co = re.findall(r'working with ([\w\.-]+)',str)
+	print(co[0])
+	degree = re.findall(r'completed my ([\w\.-]+) from', str)
+	print(degree[0])
+	college = re.findall(r'from ([\w\.-]+)', str)
+	print(college[0])
+	ctc = re.findall(r'currently have CTC of \$(\d{1,6})', str)
+	print(ctc[0])
+
 
 
 regex_processing()
