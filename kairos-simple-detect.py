cb22dad30dca6271417698976492b9e47d39740e
Kashyap Murali <katchu11@gmail.com>
2018-03-17 12:22:23 -0400

    Exploring Kairos

---
+++ kairos-simple-detect.py
@@ -0,0 +1,16 @@
+
+import requests
+
+# put your keys in the header
+headers = {
+    "app_id": "b974ce92",
+    "app_key": "d5c6c9067bf7c769d66598d9452db856"
+}
+
+payload = '{"image":"https://media.kairos.com/liz.jpg"}'
+
+url = "http://api.kairos.com/detect"
+
+# make request
+r = requests.post(url, data=payload, headers=headers)
+print (r.content)
